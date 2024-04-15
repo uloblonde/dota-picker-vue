bb164e6ccdf0f8310c66dad20eca1586e0a04572
Robert Mahendra <43535409+uloblonde@users.noreply.github.com>
2024-04-15 21:01:29 +0700

    feat : tailwind css

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -17,6 +17,9 @@ export default defineComponent({
   data() {
     return {
       heroes: [] as Hero[],
+      searchQuery: "",
+      currentPage: 1,
+      heroesPerPage: 10,
     };
   },
   async created() {
@@ -33,19 +36,48 @@ export default defineComponent({
       console.error(error);
     }
   },
+  computed: {
+    paginatedHeroes() {
+      const startIndex = (this.currentPage - 1) * this.heroesPerPage;
+      return this.filterHeroes().slice(startIndex, startIndex + this.heroesPerPage);
+    },
+  },
+  methods: {
+    filterHeroes() {
+      return this.heroes.filter((hero) => {
+        const name = hero.name.toLowerCase();
+        const nameLoc = hero.name_loc.toLowerCase();
+        const searchQuery = this.searchQuery.toLowerCase();
+        return name.includes(searchQuery) || nameLoc.includes(searchQuery);
+      });
+    },
+    paginate(page: any) {
+      this.currentPage = page;
+    },
+  },
 });
 </script>
 
 <template>
   <div>
     <h1>Dota 2 Heroes</h1>
+    <input v-model="searchQuery" placeholder="Search by name or name_loc" />
     <ul>
-      <li v-for="hero in heroes" :key="hero.id">
-        <img :src="hero.image" :alt="hero.name_loc" />
-        <h2>{{ hero.name_loc }}</h2>
-      </li>
+      <div class="grid grid-cols-4 gap-4">
+        <li v-for="hero in paginatedHeroes" :key="hero.id">
+          <div>
+            <img :src="hero.image" :alt="hero.name_loc" class="w-1/2 h-1/2 align-center mx-auto" />
+            <h2>{{ hero.name_loc }}</h2>
+          </div>
+        </li>
+      </div>
     </ul>
+    <div>
+      <button @click="paginate(1)" :disabled="currentPage === 1">First</button>
+      <button @click="paginate(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      <span>{{ currentPage }} of {{ Math.ceil(filterHeroes().length / heroesPerPage) }}</span>
+      <button @click="paginate(currentPage + 1)" :disabled="currentPage === Math.ceil(filterHeroes().length / heroesPerPage)">Next</button>
+      <button @click="paginate(Math.ceil(filterHeroes().length / heroesPerPage))" :disabled="currentPage === Math.ceil(filterHeroes().length / heroesPerPage)">Last</button>
+    </div>
   </div>
 </template>
-
-
